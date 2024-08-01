218a54ed580c2b1713f843d699d3706d63fc55ae
DelRiscoAriel <69070833+DelRiscoAriel@users.noreply.github.com>
2024-08-01 10:28:18 -0400

    Update settings.py

--- LittleLemon_Final/settings.py
+++ LittleLemon_Final/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'LittleLemon_Final.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['LittleLemonAPI\Templates'],
+        'DIRS': ['LittleLemonAPI/Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
